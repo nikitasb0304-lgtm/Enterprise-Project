f5fc2186428c68ce1a1fa51100a1d341c477ace1
Nikita Bhandare <nikitasb0304@gmail.com>
2026-01-08 20:48:07 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
